b6a4b30c216d36619c2c1e199b4eb2915b1434cc
bruno <lonbruno94@gmail.com>
2022-11-07 18:05:15 +0100

    appel des fonction dans le main

--- main.f90
+++ main.f90
@@ -2,14 +2,15 @@
                                    PROGRAM MAIN 
 !------------------------------------------------------------------------
 
-USE CONSTANTES
+USE MODULE_DECLARATIONS
 
 IMPLICIT NONE
 
 
 !--- Appel des fonctions
-CALL CALCUL_CONSTANTES
-CALL AFFICHAGE           !affichage des paramètres en INPUT et des constantes du système
+CALL APPEL_PARAM_INPUT()
+CALL CALCUL_CONSTANTES()
+CALL AFFICHAGE()           !affichage des paramètres en INPUT et des constantes du système
 
 !------------------------------------------------------------------------
                                  END PROGRAM MAIN 
