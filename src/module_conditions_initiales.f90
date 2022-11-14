1bb200d413423a424985a24096b8c463d9cc9d29
aborderies <antonin.borderies@obspm.fr>
2022-11-14 17:29:19 +0000

    Modification du USE

--- src/module_conditions_initiales.f90
+++ src/module_conditions_initiales.f90
@@ -5,7 +5,7 @@ MODULE CONDITIONS_INITIALES
 !> - Les conditions initiales sur T_AD, S_AD et H_AD.
 !===================================================================================================
 
-    USE MODULE module_integration 
+    USE MODULE_DECLARATIONS
     IMPLICIT NONE
     
 
