d8a5ecd1aebaa999579a7f15c0ccee6511db026c
Quentin <quentin.moysan@obspm.fr>
2022-11-14 17:49:42 +0100

    modification de dichotomie et S_curve

--- S_curve.f90
+++ S_curve.f90
@@ -4,23 +4,32 @@ USE module_fonctions_utiles
 USE module_branche_epais
 IMPLICIT NONE
 
-real(kind=xp), DIMENSION(n)   :: T_epais
-real(kind=xp), DIMENSION(n)   :: T_mince
-real(kind=xp), DIMENSION(n)   :: S_epais
-real(kind=xp), DIMENSION(n)   :: S_mince
-real(kind=xp)                 :: Sd
-real(kind=xp)                 :: Sg
-real(kind=xp)                 :: H
-integer                       :: i
+!Déclarations
 
-T_epais(1)=0.1
-DO i=2,n
-   T_epais(i)=T_epais(i-1)+0.1   
-ENDDO
+REAL(KIND=xp), DIMENSION(n,nx)   :: T_epais
+REAL(KIND=xp), DIMENSION(n,nx)   :: T_mince
+REAL(KIND=xp), DIMENSION(n,nx)   :: S_epais
+REAL(KIND=xp), DIMENSION(n,nx)   :: S_mince
+REAL(KIND=xp)                    :: Sd
+REAL(KIND=xp)                    :: Sg
+REAL(KIND=xp)                    :: H
+INTEGER                          :: i,j
 
-T_mince(1)=0.1
-DO i=2,n
-   T_mince(i)=T_mince(i-1)+0.1
+DO j=1,nx
+
+   T_epais(1,j)=0.1
+   DO i=2,n
+      T_epais(i,j)=T_epais(i-1,j)+0.1   
+   ENDDO
+
+   T_mince(1,j)=0.1
+   DO i=2,n
+      T_mince(i,j)=T_mince(i-1,j)+0.1
+   ENDDO
+
+   DO i=1,n
+      
+   ENDDO
 ENDDO
 
 
