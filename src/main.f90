1d8c9efd4647d8777b0e9207ca2c470f84d34ba3
bruno <lonbruno94@gmail.com>
2022-11-28 18:20:22 +0100

    main

--- src/main.f90
+++ src/main.f90
@@ -1,17 +1,16 @@
 !========================================================================
 PROGRAM MAIN 
 !========================================================================
-
 USE MODULE_DECLARATIONS
 USE MODULE_FUNCTION
 USE MODULE_CONDITIONS_INITIALES
 USE MODULE_ECRITURE
-USE MODULE_SCHEMAS_T
 USE MODULE_SCHEMAS_SIGMA
+USE MODULE_SCHEMAS_T
 
 IMPLICIT NONE
 
-INTEGER :: I
+INTEGER :: I,J
 
 !-----------------------------------------------------------------------
 !-- INITIALISATION
@@ -27,11 +26,9 @@ CALL CREATION_CONDITIONS_INITIALES()
 !TEMP_AD = 1.0E-2_XP
 !S_AD = 1.0E+02_XP
 
-!TEMP_AD = TEMP_AD_INI
-!S_AD = S_AD_INI
+TEMP_AD = TEMP_AD_INI
+S_AD = S_AD_INI
 
-TEMP_AD = 1E68
-S_AD    = 
 !-----------------------------------------------------------------------
 !-- CALCUL L'ETAT DU DISQUE 
 !-----------------------------------------------------------------------
@@ -41,21 +38,28 @@ CALL COMPUTE_EQS()
 !-- INITIALISER VECTEUR DES SCHEMA EXPLICITES-----
 CALL CREER_LAMBDA()
 
+DELTA_T_TH=0.001*DELTA_T_VISQ
+
 DO I=1,20
     
     
     !---APPEL DU SCHEMA NUMÉRIQUES
     CALL SCHEMA_IMPLICITE_S(NU_AD)
-    CALL SCHEMA_EULER(TEMP_AD,
     
-    !-- CALCUL ETAT SYSTÈME
-    CALL COMPUTE_EQS()
+    DO J=1,1000
+        CALL ITERATION_TEMP_AD()
+        
+        CALL COMPUTE_EQS()
+        IF (MOD(100,J)==1) THEN
+        CALL ECRITURE_AD_2(101,I)
+        ENDIF
+    ENDDO
     
     
     !-----------------------------------------------------------------------
     !-- SORTIES ADIMENSIONNEES
     !-----------------------------------------------------------------------
-    CALL ECRITURE_AD_2(101)
+    
     
 ENDDO
 
