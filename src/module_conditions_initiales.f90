269d4b5b57dea32c4910ebce5b8cd42c9cbeb6c3
aborderies <antonin.borderies@obspm.fr>
2022-11-28 14:44:31 +0000

    Mise en forme

--- src/module_conditions_initiales.f90
+++ src/module_conditions_initiales.f90
@@ -21,15 +21,15 @@ MODULE MODULE_CONDITIONS_INITIALES
     !---------------------------------------------------------------------------------------------------
         IMPLICIT NONE 
         
-        F_RG = 1.0_XP - (X_AD**2.0_XP/3.0_XP)**(-1._XP/2._XP)
+        F_RG = 1.0_XP - (X_AD**2.0_XP/3.0_XP)**(-1.0_XP/2.0_XP)
 
         TEMP_AD_INI = 1.4E4_XP / T_0 * ALPHA**(-1.0_XP/5._XP)  * F_RG**(3.0_XP/10.0_XP)                   &
                     * (M_0_DOT * M_0_DOT_FRAC / (1E13_XP))**(3.0_XP/10.0_XP) * (M/M_O)**(1.0_XP/4.0_XP)   &
                     * (R_S/1E8_XP)**(-3._XP/4._XP) * X_AD**(-3.0_XP/2.0_XP) 
 
         S_AD_INI    = 52.0_XP / S_0 * ALPHA**(-4.0_XP/5.0_XP) * F_RG**(7.0_XP/10.0_XP)                    &
-                    * (M_0_DOT * M_0_DOT_FRAC / (10E13_XP))**(7.0_XP/10.0_XP) * (M/M_O)**(1.0_XP/4.0_XP)  &
-                    * (R_S / 10.0_XP**8._XP)**(-3.0_XP/4.0_XP) * X_AD**(-1.0_XP/2.0_XP)
+                    * (M_0_DOT * M_0_DOT_FRAC / (1E13_XP))**(7.0_XP/10.0_XP) * (M/M_O)**(1.0_XP/4.0_XP)  &
+                    * (R_S / 1E8)**(-3.0_XP/4.0_XP) * X_AD**(-1.0_XP/2.0_XP)
 
         H_AD_INI    = 1.7E-2_XP   * ALPHA**(-1.0_XP/10.0_XP) * F_RG**(3.0_XP/5.0_XP)                   &
                     * (M_0_DOT*M_0_DOT_FRAC/(1E13_XP))**(3.0_XP/20.0_XP) * (M/M_O)**(-3.0_XP/5.0_XP)  &
