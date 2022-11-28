f6c5ff9aa5f2473542a67a1987fcc20399c40dec
Quentin <quentin.moysan@obspm.fr>
2022-11-28 18:11:29 +0100

    Ajout de l'article et changements dans dicho et scurve

--- src/S_curve.f90
+++ src/S_curve.f90
@@ -6,7 +6,7 @@ IMPLICIT NONE
 
 !Déclarations
 
-INTEGER,       PARAMETER      :: n=1
+INTEGER,       PARAMETER      :: n=100
 REAL(KIND=xp), DIMENSION(n)   :: T_epais
 REAL(KIND=xp), DIMENSION(n)   :: T_mince
 REAL(KIND=xp), DIMENSION(n)   :: S_epais
@@ -21,21 +21,23 @@ CALL APPEL_PARAM_INPUT()
 CALL CALCUL_CONSTANTES()
 OPEN (unit=10,file="results.out",status="unknown")
 
-Tmin=1E-2
-Tmax=1E-1
-Sa=1E2
-Sb=1E10
+Tmin=1.32_xp
+Tmax=8.37_xp
+Sa=1E1
+Sb=1E4
 j=30
 
 T_epais(1)=Tmin
+PRINT*, "T= ", T_epais(1)
 CALL dichotomie(T_epais(1),Sa,Sb,j,mince,Sc)
 WRITE(10,*) T_epais(1), Sc
 S_epais(1)=Sc
 
 DO i=2,n
-   Sa=1E-3
-   Sb=1E10
+   Sa=1E1
+   Sb=1E4
    T_epais(i)=T_epais(i-1)+(Tmax-Tmin)/n
+   PRINT*, "T= ", T_epais(i)
    CALL dichotomie(T_epais(i),Sa,Sb,j,mince,Sc)
    WRITE(10,*) T_epais(i), Sc
    S_epais(i)=Sc
