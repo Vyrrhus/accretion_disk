9ca2570213da925c9b7caaa7159c6b9b88e6a67b
Come Tabary <ctabary@obspm.fr>
2022-12-05 14:17:27 +0100

    tests des schemas avec pas de temps visqueux et thermiques

--- src/program_schema.f90
+++ src/program_schema.f90
@@ -6,7 +6,7 @@ USE MODULE_DECLARATIONS
 USE MODULE_SCHEMAS_SIGMA
 USE MODULE_FUNCTION
 USE MODULE_CONDITIONS_INITIALES
-
+USE MODULE_SCHEMAS_T
 
 IMPLICIT NONE
 
@@ -18,10 +18,16 @@ INTEGER :: i_temps
 CALL APPEL_PARAM_INPUT()
 CALL CALCUL_CONSTANTES()
 CALL CREATION_CONDITIONS_INITIALES()
-
+TEMP_AD=TEMP_AD_INI
+S_AD=S_AD_INI
+CALL COMPUTE_EQS()
 S_AD=S_AD_INI
 
-NU_TEST=1.0_xp
+NU_TEST=SUM(NU_AD)/NX
+
+PRINT*, 'nu moyen', NU_TEST(1)
+PRINT*, 'final expected value', 0.5_xp*(10.0_xp-SQRT(3.0_xp))/NU_TEST(1)
+DELTA_T_VISQ=0.005*10000/(SUM(NU_AD)/NX)
 
 CALL CREER_LAMBDA()
 
@@ -45,6 +51,25 @@ DO i_temps = 1,1000
     CALL SCHEMA_CN_S(NU_TEST, 0.7_xp)
 END DO
 CLOSE(2)
+
+
+PRINT *, 't_visq', 10000/(SUM(NU_AD)/NX)
+
+PRINT *, 't_therm', 10000/((SUM(NU_AD)/NX))*(SUM(H_AD_INI)/NX/100)**2
+
+PRINT *, 'R/H', 100/(SUM(H_AD_INI)/NX)
+
+DELTA_T_TH=0.000001*10000/((SUM(NU_AD)/NX))*(SUM(H_AD_INI)/NX/100)**2
+
+OPEN(3, file='test_temp.out')
+WRITE(3,*) X_AD
+DO i_temps = 1,10000
+    WRITE(3,*) TEMP_AD
+    CALL ITERATION_TEMP_AD
+    CALL COMPUTE_EQS
+END DO
+CLOSE(3)
+
 !------------------------------------------------------------------------
                                  END PROGRAM TEST_SCHEMA
 !------------------------------------------------------------------------
