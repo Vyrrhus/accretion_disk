22bd6489c028431471fe6ab735d2f7ac9d0bd4bd
Quentin <quentin.moysan@obspm.fr>
2022-12-05 15:06:24 +0100

    Changement des noms des variables

--- src/S_curve.f90
+++ src/S_curve.f90
@@ -7,8 +7,8 @@ IMPLICIT NONE
 !Déclarations
 
 INTEGER,       PARAMETER      :: n=100
-REAL(KIND=xp), DIMENSION(n)   :: T_epais
-REAL(KIND=xp), DIMENSION(n)   :: T_mince
+REAL(KIND=xp), DIMENSION(n)   :: Temp_epais
+REAL(KIND=xp), DIMENSION(n)   :: Temp_mince
 REAL(KIND=xp), DIMENSION(n)   :: S_epais
 REAL(KIND=xp), DIMENSION(n)   :: S_mince
 INTEGER                       :: j, i
@@ -19,35 +19,67 @@ LOGICAL                       :: mince=.false.
 
 CALL APPEL_PARAM_INPUT()
 CALL CALCUL_CONSTANTES()
-OPEN (unit=10,file="results.out",status="unknown")
+OPEN (unit=10,file="results_epais.out",status="unknown")
+OPEN (unit=11,file="results_mince.out",status="unknown")
 
-Tmin=1.32_xp
-Tmax=8.37_xp
+Temp_min=1.32_xp
+Temp_max=4.09_xp
 Sa=1E1
 Sb=1E4
 j=30
 
-T_epais(1)=Tmin
-PRINT*, "T= ", T_epais(1)
-CALL dichotomie(T_epais(1),Sa,Sb,j,mince,Sc)
-WRITE(10,*) T_epais(1), Sc
+Temp_epais(1)=Temp_min
+PRINT*, "T= ", Temp_epais(1)
+CALL dichotomie(Temp_epais(1),Sa,Sb,j,mince,Sc)
+WRITE(10,*) Temp_epais(1), Sc
 S_epais(1)=Sc
 
 DO i=2,n
    Sa=1E1
    Sb=1E4
-   T_epais(i)=T_epais(i-1)+(Tmax-Tmin)/n
-   PRINT*, "T= ", T_epais(i)
-   CALL dichotomie(T_epais(i),Sa,Sb,j,mince,Sc)
-   WRITE(10,*) T_epais(i), Sc
+   Temp_epais(i)=Temp_epais(i-1)+(Temp_max-Temp_min)/n
+   PRINT*, "T= ", Temp_epais(i)
+   CALL dichotomie(Temp_epais(i),Sa,Sb,j,mince,Sc)
+   WRITE(10,*) Temp_epais(i), Sc
    S_epais(i)=Sc
 ENDDO
 
-
-PRINT*, T_epais
+PRINT*, Temp_epais
 PRINT*, S_epais
 
-
 CLOSE(10)
+
+PRINT*, "Branche mince"
+
+Temp_min=4.09_xp
+Temp_max=10.28_xp
+Sa=1E-20
+Sb=1E20
+j=30
+mince=.true.
+
+Temp_mince(1)=Temp_min
+PRINT*, "T= ", Temp_mince(1)
+CALL dichotomie(Temp_mince(1),Sa,Sb,j,mince,Sc)
+WRITE(11,*) Temp_mince(1), Sc
+S_mince(1)=Sc
+
+DO i=2,n
+   Sa=1E1
+   Sb=1E4
+   Temp_mince(i)=Temp_mince(i-1)+(Temp_max-Temp_min)/n
+   PRINT*, "T= ", Temp_mince(i)
+   CALL dichotomie(Temp_mince(i),Sa,Sb,j,mince,Sc)
+   WRITE(11,*) Temp_mince(i), Sc
+   S_mince(i)=Sc
+ENDDO
+
+
+PRINT*, Temp_mince
+PRINT*, S_mince
+
+
+
+CLOSE(11)
 
 END PROGRAM courbe_S
