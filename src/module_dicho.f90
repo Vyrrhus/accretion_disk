8f2002d483e00527e17e7a6d6ef5b840f3b0513f
Quentin <quentin.moysan@obspm.fr>
2023-01-02 14:50:01 +0100

    Ajout du compteur

--- src/module_dicho.f90
+++ src/module_dicho.f90
@@ -5,7 +5,7 @@ IMPLICIT NONE
 
 CONTAINS
 
-SUBROUTINE dichotomie(Temp_S, Sa, Sb, p, mince, Sc)
+SUBROUTINE dichotomie(Temp_S, Sa, Sb, ipos, mince, Sc, ecriture)
 ! --------------------------------------------------------------------------------------------------------------------------------------
 !Calcul du zéro de la fonction Q+=Q- pour les deux branches.
 ! --------------------------------------------------------------------------------------------------------------------------------------
@@ -15,7 +15,8 @@ SUBROUTINE dichotomie(Temp_S, Sa, Sb, p, mince, Sc)
    REAL(KIND=xp), INTENT(inout):: Sa, Sb                                          !! Points de départ de la dichotomie
    LOGICAL,       INTENT(in)   :: mince                                           !! Booléen pour savoir dans quelle branche on est
    REAL(KIND=xp), INTENT(out)  :: Sc                                              !! Point milieu de la dichotomie
-   INTEGER,       INTENT(in)   :: p                                               !! Indice de la position
+   INTEGER,       INTENT(in)   :: ipos                                               !! Indice de la position
+   LOGICAL,       INTENT(inout):: ecriture 
  
    REAL(KIND=xp)               :: prec=200._xp                                     !! Précision de la dichotomie
    REAL(KIND=xp)               :: eps
@@ -31,72 +32,75 @@ SUBROUTINE dichotomie(Temp_S, Sa, Sb, p, mince, Sc)
    REAL(KIND=xp)               :: Fza, Fzb, Fzc                                   !! Fz aux point a, b et c 
    REAL(KIND=xp)               :: Fa, Fb, Fc                                      !! Fonction à annuler aux points a, b et c
    REAL(KIND=xp)               :: Kffa,Kffb, Kffc                                 !! Kff aux points a, b et c
+   INTEGER                     :: counter
 
 
    eps=300._xp
+
+   counter=0
    
-   PRINT*, "x= ", X_AD(p)
+   !PRINT*, "x= ", X_AD(ipos)
 
    Prad =  Temp_S ** 4._xp                                                                               !Calcul de Prad
-   PRINT*, "Prad= ", Prad
+   !PRINT*, "Prad= ", Prad
 
-   Omega_S = 3._xp ** (3._xp/2._xp) * X_AD(p) ** (-3._xp)
-   PRINT*, "Omega= ", Omega_S
+   Omega_S = 3._xp ** (3._xp/2._xp) * X_AD(ipos) ** (-3._xp)
+   !PRINT*, "Omega= ", Omega_S
 
-   CALL calc_H(Temp_S,x_ad(p),Omega_S,Sa,Ha)                                                                    !calcul de H au point a
-   PRINT*, "H= ", Ha
+   CALL calc_H(Temp_S,x_ad(ipos),Omega_S,Sa,Ha)                                                                    !calcul de H au point a
+   !PRINT*, "H= ", Ha
    
-   CALL calc_H(Temp_S,x_ad(p),Omega_S,Sb,Hb)                                                                    !calcul de H au point b
+   CALL calc_H(Temp_S,x_ad(ipos),Omega_S,Sb,Hb)                                                                    !calcul de H au point b
 
-   rho_a = Sa / ( x_ad(p) * Ha )                                                                      !calcul de rho au point a
-   PRINT*, "rho= ", rho_a
+   rho_a = Sa / ( x_ad(ipos) * Ha )                                                                      !calcul de rho au point a
+   !PRINT*, "rho= ", rho_a
 
-   rho_b = Sb / ( x_ad(p) * Hb )                                                                       !calcul de rho au point b
+   rho_b = Sb / ( x_ad(ipos) * Hb )                                                                       !calcul de rho au point b
 
    Pgaz_a = Temp_S * rho_a                                                                             !calcul de Pgaz au point a
-   PRINT*, "Pgaz= ", Pgaz_a
+   !PRINT*, "Pgaz= ", Pgaz_a
 
    Pa=( P_gaz_0 / P_0 ) * Pgaz_a+ ( P_rad_0 / P_0 ) * Prad
-   PRINT*, "Ptot= ", Pa
+   !PRINT*, "Ptot= ", Pa
 
    Pgaz_b = Temp_S * rho_b                                                                            !calcul de Pgaz au point b
 
    Pb=( P_gaz_0 / P_0 ) * Pgaz_b+ ( P_rad_0 / P_0 ) * Prad
 
    nua = 0.5_xp * Omega_S * Ha * Ha
-   PRINT*, "nu= ", nua
+   !PRINT*, "nu= ", nua
 
    nub = 0.5_xp *Omega_S * Hb * Hb
 
    IF (mince .eqv. .true.) THEN                                                                               !Calculs pour la branche mince
 
       Fza=F_Z_RAD_0 * rho_a**2._xp * SQRT(Temp_S) * Ha
-      PRINT*, "Fza= ", Fza
+      !PRINT*, "Fza= ", Fza
 
       Fzb=F_Z_RAD_0 * rho_b**2._xp * SQRT(Temp_S) * Hb
-      PRINT*, "Fzb= ", Fzb
+      !PRINT*, "Fzb= ", Fzb
 
       Q_plus_a = nua * Omega_S**2._xp *Q_PLUS_0
       Q_plus_b = nub * Omega_S**2._xp *Q_PLUS_0
 
-      Q_moins_a = 2._xp * x_ad(p) * Fza / (Sa*S_0)
-      Q_moins_b = 2._xp * x_ad(p) * Fzb / (Sb*S_0)
+      Q_moins_a = 2._xp * x_ad(ipos) * Fza / (Sa*S_0)
+      Q_moins_b = 2._xp * x_ad(ipos) * Fzb / (Sb*S_0)
 
       Fa = Q_plus_a - Q_moins_a
-      PRINT*, "Fa= ", Fa
+      !PRINT*, "Fa= ", Fa
 
       Fb = Q_plus_b - Q_moins_b
 
-      PRINT*, "Q+= ", Q_plus_a
-      PRINT*, "Q- mince= ", Q_moins_a
+      !PRINT*, "Q+= ", Q_plus_a
+      !PRINT*, "Q- mince= ", Q_moins_a
       
 
-      DO WHILE(eps>prec)
+      DO WHILE(eps>prec .and. counter<100)
 
          Sc=(Sa+Sb)/2._xp
-         CALL calc_H(Temp_S,x_ad(p),Omega_S,Sc,Hc)                                                              !calcul de H au point c
+         CALL calc_H(Temp_S,x_ad(ipos),Omega_S,Sc,Hc)                                                              !calcul de H au point c
 
-         rho_c = Sc / ( x_ad(p) * Hc )                                                                             !calcul de rho au point c
+         rho_c = Sc / ( x_ad(ipos) * Hc )                                                                             !calcul de rho au point c
 
          Pgaz_c = Temp_S * rho_c                                                                 !calcul de Pgaz au point c
 
@@ -108,7 +112,7 @@ SUBROUTINE dichotomie(Temp_S, Sa, Sb, p, mince, Sc)
 
          Q_plus_c = nuc * Omega_S**2._xp *Q_PLUS_0
 
-         Q_moins_c = 2._xp * x_ad(p) * Fzc / (Sc*S_0)
+         Q_moins_c = 2._xp * x_ad(ipos) * Fzc / (Sc*S_0)
 
          Fc = Q_plus_c - Q_moins_c
 
@@ -141,42 +145,43 @@ SUBROUTINE dichotomie(Temp_S, Sa, Sb, p, mince, Sc)
          ENDIF
 
          eps=ABS(Fc)                                                                                     !Calcul de l'erreur
+         counter = counter + 1
 
       ENDDO
    ELSE                                                                                                  !Calculs pour la branche épais
 
       Kffa = 6.13E18 *rho_a * Temp_S ** (-7._xp/2._xp) *rho_0 * TEMP_0 ** (-7._xp/2._xp)
-      PRINT*, "Kff= ", Kffa
+      !PRINT*, "Kff= ", Kffa
 
       Kffb = 6.13E18 *rho_b * Temp_S ** (-7._xp/2._xp) *rho_0 * TEMP_0 ** (-7._xp/2._xp)
 
-      Fza = F_Z_DIFF_0 * X_AD(p) * Temp_S**4._xp /( (KAPPA_E + Kffa) *Sa )
-      PRINT*, "Fz= ", Fza
+      Fza = F_Z_DIFF_0 * X_AD(ipos) * Temp_S**4._xp /( (KAPPA_E + Kffa) *Sa )
+      !PRINT*, "Fz= ", Fza
 
-      Fzb = F_Z_DIFF_0 * X_AD(p) * Temp_S**4._xp /( (KAPPA_E + Kffb) *Sb ) 
+      Fzb = F_Z_DIFF_0 * X_AD(ipos) * Temp_S**4._xp /( (KAPPA_E + Kffb) *Sb ) 
 
       Q_plus_a = nua * Omega_S**2._xp *Q_PLUS_0
       Q_plus_b = nub * Omega_S**2._xp *Q_PLUS_0
 
-      Q_moins_a = 2._xp * x_ad(p) * Fza / (Sa*S_0)
-      Q_moins_b = 2._xp * x_ad(p) * Fzb / (Sb*S_0)
+      Q_moins_a = 2._xp * x_ad(ipos) * Fza / (Sa*S_0)
+      Q_moins_b = 2._xp * x_ad(ipos) * Fzb / (Sb*S_0)
 
       Fa = Q_plus_a - Q_moins_a
       Fb = Q_plus_b - Q_moins_b
 
-      PRINT*, "Q+a= ", Q_plus_a
-      PRINT*, "Q-a epais=", Q_moins_a
-      PRINT*, "Q+b= ", Q_plus_b
-      PRINT*, "Q-b= ", Q_moins_b
-      PRINT*, "Fa= ", Fa
+      !PRINT*, "Q+a= ", Q_plus_a
+      !PRINT*, "Q-a epais=", Q_moins_a
+      !PRINT*, "Q+b= ", Q_plus_b
+      !PRINT*, "Q-b= ", Q_moins_b
+      !PRINT*, "Fa= ", Fa
 
-      DO WHILE(eps>prec)
+      DO WHILE(eps>prec .and. counter<100)
 
          Sc=(Sa+Sb)/2._xp
          
-         CALL calc_H(Temp_S,x_ad(p),Omega_S,Sc,Hc)
+         CALL calc_H(Temp_S,x_ad(ipos),Omega_S,Sc,Hc)
 
-         rho_c = Sc / ( x_ad(p) * Hc )                                                                 !calcul de rho au point c
+         rho_c = Sc / ( x_ad(ipos) * Hc )                                                                 !calcul de rho au point c
 
          Pgaz_c = Temp_S * rho_c
 
@@ -186,11 +191,11 @@ SUBROUTINE dichotomie(Temp_S, Sa, Sb, p, mince, Sc)
 
          Kffc = 6.13E18 *rho_c * Temp_S ** (-7._xp/2._xp) *rho_0 * TEMP_0 ** (-7._xp/2._xp)
 
-         Fzc = F_Z_DIFF_0 * X_AD(p) * Temp_S**4._xp /( (KAPPA_E + Kffc) *Sc )
+         Fzc = F_Z_DIFF_0 * X_AD(ipos) * Temp_S**4._xp /( (KAPPA_E + Kffc) *Sc )
 
          Q_plus_c = nuc * Omega_S**2._xp *Q_PLUS_0
          
-         Q_moins_c = 2._xp * x_ad(p) * Fzc / (Sc*S_0)
+         Q_moins_c = 2._xp * x_ad(ipos) * Fzc / (Sc*S_0)
          
          Fc = Q_plus_c - Q_moins_c
 
@@ -225,12 +230,20 @@ SUBROUTINE dichotomie(Temp_S, Sa, Sb, p, mince, Sc)
          ENDIF
 
          eps=ABS(Fc)
+         counter = counter + 1
 
       ENDDO
 
    ENDIF
 
-PRINT*, "Sc= ", Sc
+!PRINT*, "Sc= ", Sc
+
+IF (counter == 100) THEN
+   ecriture = .false.
+ELSE
+   ecriture = .true.
+ENDIF
+
 
 END SUBROUTINE
 
