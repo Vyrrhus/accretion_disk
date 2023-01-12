02f3626691cf7fe718c98afa0795c96d978db8cf
bruno <lonbruno94@gmail.com>
2023-01-12 20:55:58 +0100

    indentation main.f90

--- src/main.f90
+++ src/main.f90
@@ -1,44 +1,46 @@
 !========================================================================
-	                    PROGRAM MAIN 
+                 PROGRAM MAIN 
 !========================================================================
-
-			USE MODULE_DECLARATIONS
-			USE DIMENSIONNEMENT
-			USE MODULE_FUNCTION
-			USE MODULE_CONDITIONS_INITIALES
-			USE MODULE_ECRITURE
-			USE MODULE_SCHEMAS_SIGMA
-			USE MODULE_SCHEMAS_T
-			USE MODULE_BOUCLE
-
-			IMPLICIT NONE
-
-			!---------------------------------
-			!-- INITIALISATION
-			!---------------------------------
-			CALL APPEL_PARAM_INPUT()
-			CALL CALCUL_CONSTANTES()
-			CALL INIT_FILES()
-			!---------------------------------
-			!-- CONDITIONS INITIALES
-			!---------------------------------
-			CALL CREATION_CONDITIONS_INITIALES()
-
-			TEMP_AD = TEMP_AD_INI
-			S_AD = S_AD_INI
-
-			!-- CALCUL L'ETAT DU DISQUE 
-			!----------------------------------
-			CALL COMPUTE_EQS()
-			CALL ADIM_TO_PHYSIQUE()
-			CALL ECRITURE_ADIM()
-			CALL ECRITURE_DIM()
-
-			!-- BOUCLES DE CALCULS
-			!----------------------------------
-			CALL SCHEMA_FIRST_BRANCH()
-			CALL CLOSE_OUTPUT()
+!========================================================================
+!> Programme principal
+!========================================================================
+              USE MODULE_DECLARATIONS
+              USE DIMENSIONNEMENT
+              USE MODULE_FUNCTION
+              USE MODULE_CONDITIONS_INITIALES
+              USE MODULE_ECRITURE
+              USE MODULE_SCHEMAS_SIGMA
+              USE MODULE_SCHEMAS_T
+              USE MODULE_BOUCLE
+              
+              IMPLICIT NONE
+              
+              !---------------------------------
+              !-- INITIALISATION
+              !---------------------------------
+              CALL APPEL_PARAM_INPUT()
+              CALL CALCUL_CONSTANTES()
+              CALL INIT_FILES()
+              !---------------------------------
+              !-- CONDITIONS INITIALES
+              !---------------------------------
+              CALL CREATION_CONDITIONS_INITIALES()
+              
+              TEMP_AD = TEMP_AD_INI
+              S_AD = S_AD_INI
+              
+              !-- CALCUL L'ETAT DU DISQUE 
+              !----------------------------------
+              CALL COMPUTE_EQS()
+              CALL ADIM_TO_PHYSIQUE()
+              CALL ECRITURE_ADIM()
+              CALL ECRITURE_DIM()
+              
+              !-- BOUCLES DE CALCULS
+              !----------------------------------
+              CALL SCHEMA_FIRST_BRANCH()
+              CALL CLOSE_OUTPUT()
 			
 !========================================================================
-	                  END PROGRAM MAIN 
+                      END PROGRAM MAIN 
 !========================================================================
