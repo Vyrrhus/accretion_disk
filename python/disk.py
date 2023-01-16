1682141f20678e87ea28c42ebb4ba6c71389bf4d
baptiste_desoubrie <baptiste.desoubrie@obspm.fr>
2023-01-16 12:49:20 +0100

    pyplot settings, GUI amélioré, Scurve

--- python/disk.py
+++ python/disk.py
@@ -7,11 +7,42 @@ import tkinter
 from tkinter import ttk
 import pandas as pd
 
-
 from time import time
 import io
 
-# DECORATORS
+#============================================================
+# MATPLOTLIB GLOBAL SETTINGS
+
+def init_plotting():
+    plt.rcParams['figure.figsize'] = (8, 5)
+    plt.rcParams['font.size'] = 10
+    # plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams['axes.labelsize'] = plt.rcParams['font.size']
+    plt.rcParams['axes.titlesize'] = 1.5*plt.rcParams['font.size']
+    plt.rcParams['legend.fontsize'] = plt.rcParams['font.size']
+    plt.rcParams['xtick.labelsize'] = plt.rcParams['font.size']
+    plt.rcParams['ytick.labelsize'] = plt.rcParams['font.size']
+    # plt.rcParams['savefig.dpi'] = 2*plt.rcParams['savefig.dpi']
+    plt.rcParams['xtick.major.size'] = 3
+    plt.rcParams['xtick.minor.size'] = 3
+    plt.rcParams['xtick.major.width'] = 1
+    plt.rcParams['xtick.minor.width'] = 1
+    plt.rcParams['ytick.major.size'] = 3
+    plt.rcParams['ytick.minor.size'] = 3
+    plt.rcParams['ytick.major.width'] = 1
+    plt.rcParams['ytick.minor.width'] = 1
+    plt.rcParams['legend.frameon'] = True
+    plt.rcParams['legend.loc'] = 'upper right'
+    plt.rcParams['axes.linewidth'] = 1
+
+    # plt.gca().spines['right'].set_color('none')
+    # plt.gca().spines['top'].set_color('none')
+    # plt.gca().xaxis.set_ticks_position('bottom')
+    # plt.gca().yaxis.set_ticks_position('left')
+
+#============================================================
+# TIME DECORATOR
+
 def timer(func):
     """Get runtime of decorated function"""
     def wrapper(*args, **kwargs):
@@ -21,6 +52,9 @@ def timer(func):
         print(f"Fonction {func.__name__} exécutée en {tEnd-tIni:.4f}s")
         return result
     return wrapper
+
+#============================================================
+# CLASS
 
 class SkipWrapper(io.TextIOWrapper):
     """ io.TextIOWrapper pour skip les premières lignes des fichiers .out jusqu'à match:
@@ -43,7 +77,7 @@ class SkipWrapper(io.TextIOWrapper):
 
 class DataHandler():
     @timer
-    def __init__(self, filename):
+    def __init__(self, filename, scurve_filename="S_curve_data_epais_complete.csv"):
         """
             Extract data from an output file from file : [filename]
             -----------
@@ -56,6 +90,8 @@ class DataHandler():
 
             .time_label  : name of the time variable
             .space_label : name of the space variable
+
+            .scurve : scurve data
         """
         # Path
         self.path = "output/" + filename
@@ -95,6 +131,9 @@ class DataHandler():
         data.rename(columns={0: "value"}, inplace=True)
         self.df = data
 
+        # Scurve data
+        self.scurve = DataScurve(scurve_filename)
+
     def get(self, variable, space_idx=None, time_idx=None):
         array = self.df.query(f"variables=='{variable}'").to_numpy()
         array = np.reshape(array, (self.time.shape[0], self.space.shape[0]))
@@ -111,172 +150,259 @@ class DataHandler():
         return array[time_idx, space_idx]
 
     def plot(self, **fig_kw):
-        GUI = FigureGUI(self, figsize=(10,4), dpi=100, **fig_kw)
+        GUI = FigureGUI(self, **fig_kw)
         GUI.start()
+
+class DataScurve():
+    def __init__(self, filename):
+        self.data = pd.read_csv(f"output/{filename}")
+        self.r = self.data["r"].unique()
+    
+    def get(self, radius):
+        closest_value = min(self.r, key=lambda x:abs(x-radius))
+        scurve = self.data[self.data["r"] == closest_value]
+
+        temp  = scurve["T"].to_numpy()
+        sigma = scurve["Sigma"].to_numpy()
+        return temp, sigma
 
 class FigureGUI():
     # INIT
-    def __init__(self, data, title="", hasToolbar=True, **fig_kw):
+    def __init__(self, data: DataHandler, **fig_kw):
         # Pandas dataframe
-        self.data   = data
+        self.data = data
+
+        # Plot options (default values)
+        self.hasGrid = True
+        self.x = self.data.time
+        self.y = None
+        self.xlabel = self.data.time_label
+        self.ylabel = self.data.var[0]
+        self.title  = ""
+        self.timeIdx  = None
+        self.spaceIdx = 0
 
         # Windows (and resize)
         self.root = tkinter.Tk()
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
 
-        # Title
-        self.title  = None
-        if title:
-            self.title = ttk.Label(self.root, text=title)
-            self.title.grid(row=0, column=0, padx=10, pady=10)
-        
+        # Toolbar (above)
+        self.toolbar = tkinter.Frame(self.root)
+        self.toolbar.pack(side=tkinter.TOP, fill=tkinter.X)
+        self.toolbar.columnconfigure(1, weight=2)
+        self.toolbar.columnconfigure(3, weight=2)
+
         # Matplotlib embed
         self.figure = Figure(**fig_kw)
         self.ax     = self.figure.add_subplot()
         line,       = self.ax.plot([], [])
         self.line   = line
 
-        self.canvas = FigureCanvasTkAgg(self.figure, master=self.root)
-        self.canvas.draw()
-        self.canvas.get_tk_widget().grid(row=1, column=0, ipadx=40, ipady=20)
+        self.canvas = FigureCanvasTkAgg(self.figure, self.root)
+        NavigationToolbar2Tk(self.canvas, self.root)
+        line.set_data([i for i in range(10)], [np.sin(i) for i in range(10)])
+        self.canvas.get_tk_widget().pack(fill=tkinter.BOTH, expand=True)
+
+        # Toolbar > Y-axis
+        self.YaxisLabel   = ttk.Label(self.toolbar, text="Y-axis variable").grid(row=0, column=0, sticky=tkinter.E, padx=5, pady=5)
+        self.YaxisOptions = tkinter.StringVar(self.toolbar)
+        self.YaxisMenu    = ttk.Combobox(
+            self.toolbar,
+            textvariable=self.YaxisOptions,
+            values=tuple(self.data.var),
+            state="readonly"
+        )
+        self.YaxisMenu.current(0)
+        self.YaxisMenu.grid(row=0, column=1, sticky=tkinter.W, padx=5, pady=5)
+        self.YaxisMenu.bind("<<ComboboxSelected>>", self.event_yaxis)
+
+        # Toolbar > X-axis
+        self.XaxisLabel   = ttk.Label(self.toolbar, text="X-axis variable").grid(row=0, column=2, sticky=tkinter.E, padx=5, pady=5)
+        self.XaxisOptions = tkinter.StringVar(self.toolbar)
+        self.XaxisMenu    = ttk.Combobox(
+            self.toolbar,
+            textvariable=self.XaxisOptions,
+            values=(self.data.time_label, self.data.space_label,) + tuple(self.data.var),
+            state="readonly"
+        )
+        self.XaxisMenu.current(0)
+        self.XaxisMenu.grid(row=0, column=3, sticky=tkinter.W, padx=5, pady=5)
+        self.XaxisMenu.bind("<<ComboboxSelected>>", self.event_xaxis)
+
+        # Toolbar > Sliders
+        self.TimeFrame  = ttk.Frame(self.toolbar)
+        self.TimeFrame.grid(row=1, column=0, columnspan=4, sticky=tkinter.NSEW)
+        self.TimeLabel  = ttk.Label(self.TimeFrame, text=f"{self.data.time_label} = {self.data.time[0]:.4f}")
+        self.TimeValue  = tkinter.IntVar(self.TimeFrame)
+        self.TimeSlider = ttk.Scale(
+            self.TimeFrame,
+            from_=0,
+            to=self.data.time.shape[0] - 1,
+            variable=self.TimeValue,
+            command=self.event_timeSlider
+        )
+        self.TimeLabel.pack(side=tkinter.LEFT, padx=5, pady=5)
+        self.TimeSlider.pack(side=tkinter.LEFT, padx=20, pady=5, expand=True, fill=tkinter.X)
+
+        self.SpaceFrame  = ttk.Frame(self.toolbar)
+        self.SpaceFrame.grid(row=1, column=0, columnspan=4, sticky=tkinter.NSEW)
+        self.SpaceLabel  = ttk.Label(self.SpaceFrame, text=f"{self.data.space_label} = {self.data.space[0]:.4f}")
+        self.SpaceValue  = tkinter.IntVar(self.SpaceFrame)
+        self.SpaceSlider = ttk.Scale(
+            self.SpaceFrame,
+            from_=0,
+            to=self.data.space.shape[0] - 1,
+            variable=self.SpaceValue,
+            command=self.event_spaceSlider
+        )
+        self.SpaceTextValue = tkinter.StringVar(value=self.data.space[0])
+        # self.SpaceSpinbox = ttk.Spinbox(
+        #     self.SpaceFrame,
+        #     from_=0,
+        #     to=self.data.space[-1],
+        #     textvariable=self.SpaceTextValue,
+        #     wrap=False,
+        #     values=tuple(self.data.space)
+        # )
+        self.SpaceLabel.pack(side=tkinter.LEFT, padx=5, pady=5)
+        # self.SpaceSpinbox.pack(side=tkinter.LEFT, padx=5, pady=5)
+        self.SpaceSlider.pack(side=tkinter.LEFT, padx=20, pady=5, expand=True, fill=tkinter.X)
+
+        # Configure style
+        self.style = ttk.Style(self.root)
+        # self.style.configure('TFrame', )
+        try:
+            self.style.theme_use('alt')
+        except:
+            pass
+
+    def event_xaxis(self, event):
+        self.XaxisMenu.selection_clear()
+        previous_xlabel = self.xlabel
+        self.xlabel = self.XaxisOptions.get()
+        self.ax.set_xlabel(self.xlabel)
+
+        # Switch vers Y(spatial)
+        if self.xlabel == self.data.space_label and previous_xlabel != self.data.space_label:
+            self.TimeFrame.tkraise()
+            self.spaceIdx = None
+            self.timeIdx = 0
         
-        # Toolbar enhanced
-        self.toolbar = None
-        if hasToolbar:
-            self.footer = ttk.Frame(master=self.root)
-            self.footer.grid(row=2, column=0)
+        # Switch depuis Y(spatial)
+        elif self.xlabel != self.data.space_label and previous_xlabel == self.data.space_label:
+            self.SpaceFrame.tkraise()
+            self.timeIdx = None
+            self.spaceIdx = 0
 
-            # Matplotlib default toolbar
-            toolbarFrame = tkinter.Frame(master=self.footer)
-            toolbarFrame.grid(row=0,column=0, sticky=tkinter.W)
-            self.toolbar = NavigationToolbar2Tk(self.canvas, toolbarFrame)
-            self.toolbar.update()
+        self.update()
 
-            # Combobox to select variables
-            self.optionsList  = tkinter.StringVar(self.footer)
-            self.optionsLabel = ttk.Label(self.footer, text="Variables :")
-            self.optionsMenu  = ttk.Combobox(
-                self.footer,
-                textvariable=self.optionsList,
-                values=tuple(self.data.var),
-                state="readonly",
-                width=20)
-            self.optionsMenu.bind("<<ComboboxSelected>>", self.set_variable_event)
-            self.optionsLabel.grid(column=1, row=0, sticky=tkinter.W, padx=5, pady=5)
-            self.optionsMenu.grid(column=2, row=0, sticky=tkinter.W, padx=5, pady=5)
+    def event_yaxis(self, event):
+        self.YaxisMenu.selection_clear()
+        self.ylabel = self.YaxisOptions.get()
+        self.ax.set_ylabel(self.ylabel)
+        self.update()
 
-            # Checkbox to switch between time and space as x-axis
-            self.isTimeBool     = tkinter.BooleanVar(self.footer)
-            self.isTimeCheckbox = ttk.Checkbutton(
-                self.footer,
-                text="Time",
-                variable=self.isTimeBool,
-                command=self.set_spaceTime
-            )
-            self.isTimeCheckbox.grid(column=3, row=0, sticky=tkinter.W)
+    def event_timeSlider(self, event):
+        self.timeIdx = self.TimeValue.get()
+        self.TimeLabel.config(text=f"{self.data.time_label} = {self.data.time[self.timeIdx]:.4f}")
+        self.update()
 
-            # Slider to select value
-            self.SliderValue = tkinter.IntVar(self.footer)
-            self.SliderLabel = ttk.Label(self.footer)
-            self.Slider      = ttk.Scale(
-                self.footer,
-                length=200,
-                from_=0,
-                variable=self.SliderValue,
-                command=self.set_slider_event
-            )
-            self.SliderValueLabel = ttk.Label(self.footer)
+    def event_spaceSlider(self, event):
+        self.spaceIdx = self.SpaceValue.get()
+        self.SpaceLabel.config(text=f"{self.data.space_label} = {self.data.space[self.spaceIdx]:.4f}")
+        self.update()
 
-            self.SliderLabel.grid(column=1, row=1)
-            self.Slider.grid(column=2, row=1)
-            self.SliderValueLabel.grid(column=1, row=2)
-
-    # EVENTS
-    def set_variable_event(self, event):
-        """ handle the variable change event"""
-        self.optionsMenu.selection_clear()
-        variable = self.optionsList.get()
-        self.set_variable(variable)
-
-    def set_slider_event(self, event):
-        """ handle the slider event """
-        self.update_plot()
-
-    # PLOT SETTERS
-    def set_variable(self, variable):
-        """ Set Y-axis variable, ylabel and ylim"""
-        self.variable = variable
-        self.ax.set_ylabel(variable)
-        minmax = [np.nanmin(self.data.get(variable)), np.nanmax(self.data.get(variable))]
-        yrange = minmax[1] - minmax[0]
-        self.ax.set_ylim(minmax[0] - yrange/20., minmax[1] + yrange/20.)
-        self.update_plot()
-
-    def set_spaceTime(self):
-        """ Set X-axis variable, xlabel and xlim
-            Also switch Slider time / space
-        """
-        if self.isTimeBool.get():
-            # Slider : Space
-            self.Slider["to"] = self.data.space.shape[0] - 1
-            self.SliderLabel.configure(text=self.data.space_label)
-            label  = "Time"
-            array  = self.data.time
+    def relim(self):
+        # Limits
+        if self.xlabel == self.data.time_label or self.xlabel == self.data.space_label:
+            x_array = self.x
         else:
-            # Slider : Time
-            self.Slider["to"] = self.data.time.shape[0] - 1
-            self.SliderLabel.configure(text="Time")
-            label = self.data.space_label
-            array = self.data.space
+            x_array = self.data.get(self.xlabel)
+        y_array = self.data.get(self.ylabel)
+
+        # self.line.set_data([np.nanmin(x_array), np.nanmax(x_array)], [np.nanmin(y_array), np.nanmax(y_array)])
+        # self.ax.relim()
+        # self.ax.autoscale_view()
+
+        xlims = [np.nanmin(x_array), np.nanmax(x_array)]
+        ylims = [np.nanmin(y_array), np.nanmax(y_array)]
+        xmargin = (xlims[1] - xlims[0]) * 0.05
+        ymargin = (ylims[1] - ylims[0]) * 0.05
+        self.ax.set_xlim(xlims[0] - xmargin, xlims[1] + xmargin)
+        self.ax.set_ylim(ylims[0] - ymargin, ylims[1] + ymargin)
+
+    def update(self):
+        """ Mise à jour du plot """
+        # X value
+        if self.xlabel == self.data.time_label:
+            self.x = self.data.time
+
+        elif self.xlabel == self.data.space_label:
+            self.x = self.data.space
+
+        else:
+            self.x = self.data.get(self.xlabel, space_idx=self.spaceIdx, time_idx=self.timeIdx)
         
-        self.Slider.set(0)
+        # Y value
+        self.y = self.data.get(self.ylabel, space_idx=self.spaceIdx, time_idx=self.timeIdx)
 
-        minmax = [np.nanmin(array), np.nanmax(array)]
-        xrange = minmax[1] - minmax[0]
-        self.ax.set_xlabel(label)
-        self.ax.set_xlim(minmax[0] - xrange/20., minmax[1] + xrange/20.)
-        self.update_plot()
+        # Data
+        self.relim()
+        self.line.set_data(self.x, self.y)
 
-    # START & UPDATE
-    def start(self, variable=None):
-        if not variable:
-            variable = self.data.var[0]
-        
-        self.optionsMenu.set(variable)
-        self.set_variable(variable)
-        self.set_spaceTime()
+        # Title
+        self.ax.set_title(f"{self.ylabel}({self.xlabel})")
 
+        # Grid
+        if self.hasGrid:
+            self.ax.grid(True)
+        else:
+            self.ax.grid(False)
+
+        # Additionnal custom features
+        self.custom_plot()
+
+        self.canvas.draw()
+
+    def start(self):
+        self.update()
+        self.ax.set_xlabel(self.xlabel)
+        self.ax.set_ylabel(self.ylabel)
         tkinter.mainloop()
 
-    def update_plot(self, hasGrid=True):
-        """ Check all getters and update the canvas"""
-        fixedValue = self.SliderValue.get()
+    def custom_plot(self):
+        # S-curve
+        if self.xlabel == "SIGMA" and self.ylabel == "TEMP":
+            radius = self.data.space[self.spaceIdx]
+            temp, sigma = self.data.scurve.get(radius)
+            try:
+                self.scurve_line.set_data(sigma*10, temp)
+            except:
+                line,            = self.ax.plot(sigma*10, temp, '--', label="Courbe en S")
+                self.scurve_line = line
 
-        if self.isTimeBool.get():
-            x = self.data.time
-            y = self.data.get(self.variable, space_idx=fixedValue)
-            self.ax.set_title(f"{self.variable} en fonction du temps")
-            self.SliderValueLabel.configure(text=f"{self.data.space[fixedValue]:.4f}")
+            self.ax.legend()
+
         else:
-            x = self.data.space
-            y = self.data.get(self.variable, time_idx=fixedValue)
-            self.ax.set_title(f"{self.variable} en fonction du rayon")
-            self.SliderValueLabel.configure(text=f"{self.data.time[fixedValue]:.4f}")
+            try:
+                self.scurve_line.set_data([], [])
+                _lg = self.ax.get_legend()
+                _lg.remove()
+                
+            except:
+                pass
 
-        self.line.set_data(x, y)
-
-        if hasGrid:
-            self.ax.grid(True)
-
-        self.canvas.draw()
+#============================================================
+# MAIN PROGRAM
 
 # Récupérer le fichier de sortie
-data = DataHandler("data_py.out")
+data = DataHandler("data.out")
 
 # Numpy array :
 data.space # array spatial
 data.time  # array du temps
+
 """
     data.time  => np.array 1D : valeurs temporelles
     data.space => np.array 1D: valeurs spatiales
@@ -296,5 +422,6 @@ data.time  # array du temps
         data.get("M_DOT", space_idx=-1, time_idx=-1) ==> np.array de 1 élément correspondant à M_dot au bord extérieur et au temps final
 """
 
-# START MATPLOTLIB :
+# Plot les données
+init_plotting()
 data.plot()
