e57e1a80737dd7d678bf9ea1d3dff7d5d82d57f2
aborderies <antonin.borderies@obspm.fr>
2023-01-25 11:24:03 +0000

    Programme test schema

--- src/program_schema.f90
+++ src/program_schema.f90
@@ -7,11 +7,18 @@ USE MODULE_SCHEMAS_SIGMA
 USE MODULE_FUNCTION
 USE MODULE_CONDITIONS_INITIALES
 USE MODULE_SCHEMAS_T
+USE MODULE_S_CURVE
+USE MODULE_SCHEMAS_INSTABILITE
 USE DIMENSIONNEMENT
 
 IMPLICIT NONE
 
-REAL(KIND=xp), DIMENSION(NX) :: NU_TEST
+REAL(KIND=xp), PARAMETER     :: FRACTION_DT_TH       = 1.0E-1_xp   !! Fraction du pas de temps thermique
+REAL(KIND=XP), PARAMETER     :: FRACTION_DT_VISQ     = 1.0E-4_XP   !! Fraction du pas de temps visqueux
+REAL(KIND=XP), PARAMETER     :: FRACTION_DT_INSTABLE = 1.0E-5*FRACTION_DT_VISQ   !! Fraction du pas de temps instable
+
+
+REAL(kind=xp), DIMENSION(NX) :: S_AD_SAVE
 
 INTEGER :: i_temps
 INTEGER :: j_temps
@@ -20,23 +27,24 @@ INTEGER :: j_check
 !--- Appel des fonctions
 CALL APPEL_PARAM_INPUT()
 CALL CALCUL_CONSTANTES()
+CALL INIT_FILES
+
+IF (COURBE_EN_S == 1) THEN
+    CALL S_curve(Temp_min_AD, Temp_max_AD, Sg_AD, Sd_AD, n_s)
+ ENDIF
+
 CALL CREATION_CONDITIONS_INITIALES()
-TEMP_AD=TEMP_AD_INI*2
-S_AD=S_AD_INI
 
 DO j_check=1, 3
     TEMP_AD(j_check) = TEMP_AD(4)
     S_AD(j_check) = S_AD(4)
 END DO
 
-CALL COMPUTE_EQS()
-S_AD=S_AD_INI*4
+CALL COMPUTE_EQS
 
-NU_TEST=SUM(NU_AD)/NX
 
-PRINT*, 'nu moyen', NU_TEST(1)
-PRINT*, 'final expected value', 0.5_xp*(10.0_xp-SQRT(3.0_xp))/NU_TEST(1)
-DELTA_T_VISQ=0.005*10000/(SUM(NU_AD)/NX)
+DELTA_T_TH_AD = FRACTION_DT_TH / MAXVAL(OMEGA_AD)
+DELTA_T_VISQ = FRACTION_DT_VISQ * MAXVAL( X_AD ** 4.0_xp / NU_AD ) 
 
 CALL CREER_LAMBDA()
 
@@ -44,59 +52,28 @@ CALL CREER_LAMBDA()
 
 PRINT *, 't_visq', 10000/(SUM(NU_AD)/NX)
 
-PRINT *, 't_therm', 10000/((SUM(NU_AD)/NX))*(SUM(H_AD_INI)/NX/100)**2
+PRINT *, 't_therm', 10000/((SUM(NU_AD)/NX))*(SUM(H_AD)/NX/100)**2
 
-PRINT *, 'R/H', 100/(SUM(H_AD_INI)/NX)
-
-DELTA_T_TH_AD=0.001*10000/((SUM(NU_AD)/NX))*(SUM(H_AD_INI)/NX/100)**2
+PRINT *, 'R/H', 100/(SUM(H_AD)/NX)
 
 
-OPEN(1, file='test_sigma.out')
-OPEN(3, file='test_temp.out')
-OPEN(4, file='Qvalue.out')
-OPEN(5, file='test_M_dot.out')
-WRITE(1,*) X_AD
-WRITE(3,*) X_AD
-WRITE(5,*) X_AD
-WRITE(4,*) X_AD
+
 
 CALL ADIM_TO_PHYSIQUE
-CALL SI_TO_CGS
-WRITE(3,*) TEMP
-WRITE(1,*) SIGMA
-WRITE(4,*) Q_PLUS_0*(Q_PLUS_AD - Q_MOINS_AD)
-WRITE(5,*) M_DOT
+CALL ECRITURE_DIM
 
-DO i_temps = 1,40
+
+PRINT*, '=========Début Schema Stable============'
+
+DO i_temps = 1,1000
 
     j_temps = 0
     DO WHILE (maxval(ABS(Q_PLUS_0*Q_PLUS_AD - Q_PLUS_0*Q_MOINS_AD)) > 100)
 
-        if(j_temps==0) Then
-            DELTA_T_TH_AD = DELTA_T_TH_AD/1.0e2
-        ELSE IF (j_temps==1) THEN
-            DELTA_T_TH_AD = DELTA_T_TH_AD*1.0e2
-        END IF
-
-        ! IF(j_temps==1) THEN
-        !     PRINT*, TAU_EFF(4), j_temps
-        !     PRINT*, Q_MOINS_AD(1)
-        ! END IF
 
         CALL ITERATION_TEMP_AD
 
         DO j_check = 1, NX
-        !     IF(TAU_EFF(j_check)<=1) THEN
-        !         TAU_EFF(j_check) = TAU_EFF(4)
-        !         TEMP_AD(j_check) = TEMP_AD(4)
-        !         S_AD(j_check) = S_AD(4)
-        !         print*, Q_MOINS_AD(j_check), j_check, j_temps
-        !     END IF
-
-        !     IF(TEMP_AD(j_check)<=0) THEN
-        !         TEMP_AD(j_check) = 1.0E6
-        !         print*, Q_MOINS_AD(j_check), j_check, j_temps
-        !     END IF
             IF(ISNAN(TEMP_AD(j_check))) THEN
                 print*, 'NaN', j_check, j_temps
                 TEMP_AD(j_check) = TEMP_AD(10)
@@ -115,25 +92,53 @@ DO i_temps = 1,40
     print*, i_temps, MAXVAL(MU/(R_BOLTZ*OMEGA_MAX*TEMP_0*C_V_AD) * Q_PLUS_0*(Q_PLUS_AD - Q_MOINS_AD)* &
                 DELTA_T_TH_AD/TEMP_AD),&
                 MAXVAL(Q_PLUS_0*(Q_PLUS_AD - Q_MOINS_AD)), j_temps
-    CALL ADIM_TO_PHYSIQUE
-    CALL SI_TO_CGS
-    WRITE(3,*) TEMP
-    WRITE(1,*) SIGMA
-    WRITE(4,*) Q_PLUS_0*(Q_PLUS_AD - Q_MOINS_AD)
-    WRITE(5,*) M_DOT
 
-    CALL SCHEMA_IMPLICITE_S(NU_TEST)
+    TIME_AD = TIME_AD + j_temps*DELTA_T_TH_AD
+    PRINT*, 'TIME =', TIME
+    CALL ADIM_TO_PHYSIQUE
+    CALL ECRITURE_DIM
+
+
+    IF (TIME>80 .or. i_temps==1000) THEN
+        PRINT*, 'Instabilité Q_ADV'
+        exit
+    END IF
+
+    S_AD_SAVE = S_AD
+    CALL SCHEMA_IMPLICITE_S(NU_AD)
+    CALL COMPUTE_Q_ADV_AD(DELTA_T_VISQ, S_AD_SAVE)
     CALL COMPUTE_EQS
+    TIME_AD = TIME_AD + (DELTA_T_VISQ - j_temps*DELTA_T_TH_AD)
 
     CALL ADIM_TO_PHYSIQUE
-    CALL SI_TO_CGS
-    WRITE(3,*) TEMP
-    WRITE(1,*) SIGMA
-    WRITE(4,*) Q_PLUS_0*(Q_PLUS_AD - Q_MOINS_AD)
-    WRITE(5,*) M_DOT
+    CALL ECRITURE_DIM
+
+    
 
 END DO
-CLOSE(3)
+
+
+PRINT*, '==========FIN SCHEMA STABLE============'
+
+
+DELTA_T_INSTABLE_AD = FRACTION_DT_INSTABLE * MAXVAL( X_AD ** 4.0_xp / NU_AD ) 
+CALL SETUP_SCHEMA_INSTABLE_TS(DELTA_T_INSTABLE_AD)
+PRINT*, 'Initialisation : dt_instable =', DELTA_T_INSTABLE_AD
+
+DO i_temps=1, 10**8
+    CALL SCHEMA_INSTABLE_TS(0.8_xp)
+    CALL COMPUTE_EQS
+    TIME_AD = TIME_AD + DELTA_T_INSTABLE_AD
+    CALL ADIM_TO_PHYSIQUE
+    IF (modulo(i_temps, 10**5)==0) THEN
+        CALL ECRITURE_DIM
+        DELTA_T_INSTABLE_AD = FRACTION_DT_INSTABLE * MAXVAL( X_AD ** 4.0_xp / NU_AD ) 
+        PRINT*, i_temps, 'dt_instable = ',DELTA_T_INSTABLE_AD
+        CALL SETUP_SCHEMA_INSTABLE_TS(DELTA_T_INSTABLE_AD)
+    END IF
+    
+END DO
+PRINT*, '==========END============'
 
 !------------------------------------------------------------------------
                                  END PROGRAM TEST_SCHEMA
